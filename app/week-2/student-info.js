410f78bfbe8fdfb065c8412c9217508f1d29c9cb
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-01-30 22:20:18 -0700

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Ethan Schwab</h1>
+        <Link href="https://github.com/misterslime">My Github</Link>
+      </main>
+    );
+  }
